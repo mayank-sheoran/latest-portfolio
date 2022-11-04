dac000121004465da2148a6505aa974c0a7287a1
Mayank Sheoran <mayanksheoran@Mayanks-MacBook-Air.local>
2022-11-04 08:31:51 +0530

    resume link updated 1.1

--- src/Components/Portfolio/Homepage/Homepage.js
+++ src/Components/Portfolio/Homepage/Homepage.js
@@ -51,7 +51,7 @@ const Homepage = () => {
                 <div className="homepage-download-btn">
                   {/* eslint-disable-next-line */}
                   <a
-                    href="https://drive.google.com/drive/u/1/folders/1W20ImRZNoP6ViuQiJbNVAh3VSaVdvTTn"
+                    href="https://drive.google.com/file/d/1daGUZsOrJa_6dzbo53v5uoG10S7i_x5o/view?usp=share_link"
                     className="btn-flip"
                     data-back="Download"
                     target="_blank"
